c551981b775029aec01cbc80b287c35392572639
Mohammed Falih <yunusfalih123@gmail.com>
2024-02-16 19:56:56 +0530

    finalized

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import http from 'http';
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import compression from "compression";
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
 import cors from 'cors';
-import mongoose from 'mongoose';;
+import router from './router';
+import mongoose from 'mongoose'
 
 const app = express();
 
 app.use(cors({
-    credentials: true,
-}))
+  credentials: true,
+}));
 
 app.use(compression());
 app.use(cookieParser());
@@ -19,11 +20,14 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 
 server.listen(8080, () => {
-    console.log('Server running on http://localhost:8080/')
+  console.log('Server running on http://localhost:8080/');
 });
 
-const MONGO_URL = 'mongodb+srv://Falih:falih@cluster0.uvjzmcr.mongodb.net/?retryWrites=true&w=majority';
+
+const MONGO_URL = "mongodb+srv://Falih:falih@cluster0.uvjzmcr.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
